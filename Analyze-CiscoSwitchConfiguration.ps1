3a24194cfaeb7690b8879157edf1d5971020fe09
Sam Pursglove <spursglove@gmail.com>
2018-11-13 20:06:56 +0800

    Additional updates

--- Analyze-CiscoSwitchConfiguration.ps1
+++ Analyze-CiscoSwitchConfiguration.ps1
@@ -12,7 +12,7 @@
 .NOTES
     Version 1.0.2
     Sam Pursglove
-    Last modified: 09 NOV 2018
+    Last modified: 13 NOV 2018
 #>
 
 [CmdletBinding()]
@@ -40,8 +40,10 @@ function Search-ConfigQuietly {
 }
 
   
-# extract interface information for further analysis; also shortens the size of searches of Select-String
+# extract interface information for further analysis; shortens the size of config file
+# so Select-String (i.e., grep) searches on the rest of the file are faster
 function Extract-InterfaceSection {
+    
     # $Flag is used to track when the config section for a given interface ends
     $Flag = $true
 
@@ -56,12 +58,17 @@ function Extract-InterfaceSection {
             if ($_ -notmatch "!") {
                 if ($_ -match "^interface (\w+)(\d\/\d{1,2}(\/\d{1,2})?)") {
 
-                    $Properties.Add('InterfaceSpeed',$Matches[1])
+                    $Properties.Add('InterfaceType',$Matches[1])
                     $Properties.Add('InterfaceNumber',$Matches[2])
 
-                } elseif ($_ -match "switchport mode access$") {
+                } elseif ($_ -match "^interface Vlan(\d{1,4})") {
+
+                    $Properties.Add('InterfaceType','Vlan')
+                    $Properties.Add('InterfaceNumber',$Matches[1])
+
+                }elseif ($_ -match "switchport mode (access|trunk)$") {
                     
-                    $Properties.Add('Access',$true)
+                    $Properties.Add('Mode',$Matches[1])
 
                 } elseif ($_ -match "switchport access vlan (\d{1,4})$") {
                     
@@ -71,9 +78,13 @@ function Extract-InterfaceSection {
                     
                     $Properties.Add('Trunk',$true)
 
-                } elseif ($_ -match "switchport trunk encapsulation dot1q$") {
+                } elseif ($_ -match "switchport trunk encapsulation (dot1q|isl|negotiate)$") {
                     
-                    $Properties.Add('Trunk8021q',$true)
+                    $Properties.Add('TrunkEncapsulation',$Matches[1])
+
+                } elseif ($_ -match "switchport trunk native vlan (\d{1,4})$") {
+                    
+                    $Properties.Add('TrunkNativeVlan',$Matches[1])
 
                 } elseif ($_ -match "switchport port-security$") {
                     
@@ -82,6 +93,14 @@ function Extract-InterfaceSection {
                 } elseif ($_ -match "switchport port-security mac-address sticky$") {
                     
                     $Properties.Add('StickyPort',$true)
+
+                } elseif ($_ -match "switchport port-security maximum (\d{1,4})$") {
+                    
+                    $Properties.Add('PortSecurityMax',$Matches[1])
+
+                } elseif ($_ -match "duplex (auto|full|half)$") {
+                    
+                    $Properties.Add('Duplex',$Matches[1])
 
                 } elseif ($_ -match "spanning-tree portfast$") {
                     
@@ -94,6 +113,10 @@ function Extract-InterfaceSection {
                 } elseif ($_ -match "spanning-tree bpduguard enable$") {
                     
                     $Properties.Add('BpduGuard',$true)
+
+                } elseif ($_ -match "shutdown$") {
+                    
+                    $Properties.Add('Shutdown',$true)
 
                 }
                 
@@ -126,21 +149,25 @@ Extract-InterfaceSection
 $Interfaces | Format-List -Property *
 
 $CiscoConfig = @{
-    version=                  Search-ConfigForValue "^version (\d{1,2}\.\d{1,2})$" $ConfigNoInterfaces
-    hostname=                 Search-ConfigForValue "^hostname (.+)$" $ConfigNoInterfaces
-    servicePasswordEncrypt=   Search-ConfigQuietly  "^service password-encryption$" $ConfigNoInterfaces
-    enableSecret=             Search-ConfigQuietly  "^enable secret .+$" $ConfigNoInterfaces
-    enablePassword=           Search-ConfigQuietly  "^enable password .+$" $ConfigNoInterfaces
-    userAccountsSecret=       Search-ConfigForValue "^username (\w+) .*secret .+$" $ConfigNoInterfaces
-    userAccountsPassword=     Search-ConfigForValue "^username (\w+) .*password .+$" $ConfigNoInterfaces
-    aaaNewModel=              Search-ConfigQuietly  "^aaa new-model$" $ConfigNoInterfaces
-    sshV2=                    Search-ConfigQuietly  "^ip ssh version 2$" $ConfigNoInterfaces
-    loginBanner=              Search-ConfigQuietly  "^banner (motd|login).+$" $ConfigNoInterfaces
-    
-    aaaAuthLocalEnabled=      Search-ConfigQuietly  "^aaa authentication login default local" $ConfigNoInterfaces
-    aaaAuthTacacsEnabled=     Search-ConfigQuietly  "^aaa authentication login default group tacacs+" $ConfigNoInterfaces
-    tacacsServer=             Search-ConfigQuietly  "^tacacs-server host" $ConfigNoInterfaces
-    tacacsServerIp=           Search-ConfigForValue "^tacacs-server host" $ConfigNoInterfaces
+    version=                Search-ConfigForValue "^version (\d{1,2}\.\d{1,2})$" $ConfigNoInterfaces
+    hostname=               Search-ConfigForValue "^hostname (.+)$" $ConfigNoInterfaces
+    servicePasswordEncrypt= Search-ConfigQuietly  "^service password-encryption$" $ConfigNoInterfaces
+    enableSecret=           Search-ConfigQuietly  "^enable secret .+$" $ConfigNoInterfaces
+    enablePassword=         Search-ConfigQuietly  "^enable password .+$" $ConfigNoInterfaces
+    userAccountsSecret=     Search-ConfigForValue "^username (\w+) .*secret .+$" $ConfigNoInterfaces
+    userAccountsPassword=   Search-ConfigForValue "^username (\w+) .*password .+$" $ConfigNoInterfaces
+    aaaNewModel=            Search-ConfigQuietly  "^aaa new-model$" $ConfigNoInterfaces
+    sshV2=                  Search-ConfigQuietly  "^ip ssh version 2$" $ConfigNoInterfaces
+    loginBanner=            Search-ConfigQuietly  "^banner (motd|login).+$" $ConfigNoInterfaces
+    snmpV2ReadOnly=         Search-ConfigQuietly  "^snmp community .+ RO" $ConfigNoInterfaces
+    snmpV2ReadOnlyAcl=      Search-ConfigForValue "^snmp community .+ RO (.*)$" $ConfigNoInterfaces
+    snmpV2ReadWrite=        Search-ConfigQuietly  "^snmp community .+ RW" $ConfigNoInterfaces
+    httpMgmtInterface=      Search-ConfigQuietly  "^ip http server$" $ConfigNoInterfaces
+
+    aaaAuthLocalEnabled=    Search-ConfigQuietly  "^aaa authentication login default local" $ConfigNoInterfaces
+    aaaAuthTacacsEnabled=   Search-ConfigQuietly  "^aaa authentication login default group tacacs+" $ConfigNoInterfaces
+    tacacsServer=           Search-ConfigQuietly  "^tacacs-server host" $ConfigNoInterfaces
+    tacacsServerIp=         Search-ConfigForValue "^tacacs-server host" $ConfigNoInterfaces
 }
 
 
@@ -203,17 +230,60 @@ if ($CiscoConfig.aaaNewModel) {
 
 # check if SSH v2 is enabled
 if ($CiscoConfig.sshV2) {
-    Write-Output "`tENABLED`t`tSSH v2"
+    Write-Output "`tPASS`t`tSSH v2 is enabled"
 } else {
-    Write-Output "`tDISABLED`tSSH v2"
+    Write-Output "`tFAIL`tSSH v2 is not enabled"
     Write-Verbose "SSH v2 should be enabled using the 'ip ssh version 2' command"
 }
 
 # check if a login banner message is used
 if ($CiscoConfig.loginBanner) {
-    Write-Output "`tENABLED`t`tLogin banner"
+    Write-Output "`tPASS`t`tA login warning banner is configured"
     Write-Verbose "The configuration contains a login warning banner.  Ensure the message conforms to the required banner text."
 } else {
-    Write-Output "`tDISABLED`tLogin banner"
+    Write-Output "`tFAIL`tA login warning banner is not configured"
     Write-Verbose "The configuration does not include a login warning banner.  Add the approved login warning banner text."
 }
+
+# check if SNMPv2 RO strings are used with or without an ACL
+if ($CiscoConfig.snmpV2ReadOnly) {
+    Write-Output "`tFAIL`t`tSNMPv2 Read-Only (RO) community strings are enabled"
+    Write-Verbose "SNMPv2 is an unencrypted protocol and the cleartext can be sniffed on the network.  If it must be used restrict access to the read-only strings with an access control list (ACL)."
+} elseif ($CiscoConfig.snmpV2ReadOnly -and $CiscoConfig.snmpV2ReadOnlyAcl) {
+    Write-Output "`tPASS`t`tSNMPv2 Read-Only (RO) community string is enabled and restricted with an access control list (ACL)"
+} else {
+    Write-Output "`tPASS`t`tSNMPv2 Read-Only (RO) community strings are not used"
+}
+
+# check if SNMPv2 RW strings are used
+if ($CiscoConfig.snmpV2ReadWrite) {
+    Write-Output "`tFAIL`t`tSNMPv2 Read-Write (RW) community strings are enabled"
+    Write-Verbose "SNMPv2 is an unencrypted protocol and the cleartext can be sniffed on the network.  If read-write strings are required these should be enabled using SNMPv3 with the appropriate authentication and encryption configured."
+} else {
+    Write-Output "`tPASS`t`tSNMPv2 Read-Write (RW) community strings are not used"
+}
+
+# check if the HTTP web management server is enabled
+if ($CiscoConfig.httpMgmtInterface) {
+    Write-Output "`tFAIL`t`tThe HTTP web management server is enabled"
+    Write-Verbose "HTTP is an unencrypted protocol and the cleartext can be sniffed on the network. If a web management interface is required enable the HTTPS version using the 'ip http secure-server' command."
+} else {
+    Write-Output "`tPASS`t`tThe HTTP web management server is disabled"
+}
+
+<#
+    TODO:
+        Check if access vlan is not vlan 1
+        Check if a port is configured as both an access and trunk port
+        Check if duplex is not auto
+        Check if bpdufilter and bpduguard are both enabled
+        Check if sticky ports are enabled
+            If not is the port at least shutdown
+        Check if sticky ports have more than 1 maximum MAC
+
+        Check on motd/login/exec banners
+
+        line vty login ssh stuff
+
+        snmpv3 checks
+#>
