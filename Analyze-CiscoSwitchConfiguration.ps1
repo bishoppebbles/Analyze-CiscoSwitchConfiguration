653693e7e2d58b1d746341dc088cf5a59e18825a
Sam Pursglove <spursglove@gmail.com>
2025-07-15 16:28:14 +0545

    Added a function to decrypt Cisco type 7 password hashes

--- Analyze-CiscoSwitchConfiguration.ps1
+++ Analyze-CiscoSwitchConfiguration.ps1
@@ -13,6 +13,8 @@
     Outputs failed tests and warnings only; tests that pass are not displayed
 .PARAMETER Output
     Set the analysis output delivery method: Excel (default), PowerShell console.  If Excel is not on the host system where the script it run it will not work using that output method.
+.PARAMETER Type7
+    Type7 hash(es)
 .EXAMPLE
     .\Analyze-CiscoSwitchConfiguration.ps1 -ConfigFile cisco_config.txt
     
@@ -25,27 +27,34 @@
     Get-ChildItem -Exclude *.ps1 | .\Analyze-CiscoSwitchConfiguration.ps1 -RedSeal
 
     This can be used to analyze multiple configs saved in a single directory that were obtained from RedSeal.  The results for each switch are displayed in its own workbook sheet.
+.EXAMPLE
+    .\Analyze-CiscoSwitchConfiguration.ps1 -Type7 <type7_hash1>[,<type7_hash2>]
+
+    Decodes Cisco's type 7 weak "encryption" and displays the plaintext password.
 .NOTES
     If Excel is not installed on the host system where the script is run that Output option will not work.  Use the 'Display' option for console output.
 
-    If AAA is enabled (i.e., 'aaa new-model') the Console and VTY 0-15 line analysis will likely be inaccurate.
-
     If there are incorrect config settings using both access and trunk commands and/or more complicated interface access/trunk config settings the logic of this code may be inaccurate and will require manual review.
 
-    Version 1.0.19
+    The Decrypt-Type7 function decodes Cisco's type 7 weak "encryption" and displays the plaintext password. It was ported by John Savu (with some code cleanup) from theevilbit's python script (https://github.com/theevilbit/ciscot7) which was released under the MIT license.
+    
+    Version 1.0.20
     Sam Pursglove
     James Swineford
+    John Savu (Decrypt-Type7 function)
     Last modified: 15 July 2025
 #>
 
 [CmdletBinding(DefaultParameterSetName='FailOnly')]
 param (
 
-    [Parameter(Position=0, Mandatory, ValueFromPipelineByPropertyName, HelpMessage='The saved config file of a Cisco switch')]
+    [Parameter(ParameterSetName='FailOnly', Position=0, Mandatory, ValueFromPipelineByPropertyName, HelpMessage='The saved config file of a Cisco switch')]
+    [Parameter(ParameterSetName='FailWarningOnly', Position=0, Mandatory, ValueFromPipelineByPropertyName, HelpMessage='The saved config file of a Cisco switch')]
     [Alias('FullName','Name')]
     [string]$ConfigFile,
 
-    [Parameter(HelpMessage='Provide additional config file filtering to accomodate the RedSeal output format')]
+    [Parameter(ParameterSetName='FailOnly', HelpMessage='Provide additional config file filtering to accomodate the RedSeal output format')]
+    [Parameter(ParameterSetName='FailWarningOnly', HelpMessage='Provide additional config file filtering to accomodate the RedSeal output format')]
     [switch]$RedSeal,
 
     [Parameter(ParameterSetName='FailOnly', HelpMessage='Only display failed tests')]
@@ -54,10 +63,13 @@ param (
     [Parameter(ParameterSetName='FailWarningOnly', HelpMessage='Only display failed tests and warnings')]
     [switch]$FailWarningOnly,
 
-    [Parameter(HelpMessage='Output type required')]
+    [Parameter(ParameterSetName='FailOnly', HelpMessage='Output type required')]
+    [Parameter(ParameterSetName='FailWarningOnly', HelpMessage='Output type required')]
     [ValidateSet('Display','Excel')]
-    [string]$Output = 'Excel'
+    [string]$Output = 'Excel',
 
+    [Parameter(ParameterSetName='Type7', Mandatory, HelpMessage='Type 7 weak "encryption" hash')]
+    [string[]]$Type7 = @()
 )
 
 
@@ -928,6 +940,54 @@ Begin {
         $script:objSheetInfo.Columns.Item(2).ColumnWidth = 17
     }
 
+
+    # Decodes Cisco's type 7 weak "encryption" and displays the plaintext password
+    # Ported by John Savu (April 2024) from theevilbit's python script
+     function Decrypt-Type7 {
+        param(
+            [string[]]$type7
+        )
+
+        foreach ($type in $type7) {
+
+            if (($type.Length % 2) -ne 0) {
+                Write-Output 'Valid type 7 password length must be even'
+                continue
+            }
+ 
+            $password = ''
+            $xlat = @(0x64, 0x73, 0x66, 0x64, 0x3b, 0x6b, 0x66, 0x6f, 0x41,
+                      0x2c, 0x2e, 0x69, 0x79, 0x65, 0x77, 0x72, 0x6b, 0x6c,
+                      0x64, 0x4a, 0x4b, 0x44, 0x48, 0x53, 0x55, 0x42, 0x73,
+                      0x67, 0x76, 0x63, 0x61, 0x36, 0x39, 0x38, 0x33, 0x34, 
+                      0x6e, 0x63, 0x78, 0x76, 0x39, 0x38, 0x37, 0x33, 0x32, 
+                      0x35, 0x34, 0x6b, 0x3b, 0x66, 0x67, 0x38, 0x37)
+    
+            $type -match '(^[0-9A-Fa-f]{2})([0-9A-Fa-f]+)' | Out-Null
+        
+            $s = [int]$Matches[1]
+            $e = $Matches[2]
+    
+            for ($pos = 0; $pos -lt $e.Length; $pos += 2) {
+                $magic = [convert]::ToInt32($e.Substring($pos, 2), 16)
+        
+                if ($s -le 50) {
+                    $newchar = [char]($magic -bxor $xlat[$s])
+                    $s++
+                }
+        
+                if ($s -eq 51) { $s = 0 }
+                $password += $newchar
+            }
+    
+            [pscustomobject]@{
+                Type7   = $type
+                Password= $password
+            }
+        }
+    }
+
+
     #endregion Functions
 
     #Write-Output "Starting at $(get-date)"
@@ -942,6 +1002,11 @@ Begin {
 
 
 Process {
+
+    if ($Type7) {
+        Decrypt-Type7 $Type7
+        exit
+    }
 
     Write-Progress "Processing $($ConfigFile.Split('\')[-1])..."
 
